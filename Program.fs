049c771059d19585b77db6ae6026a9fc85c73f78
J2ghz <j2.00ghz@gmail.com>
2019-11-09 00:43:38 +0100

    finish the rest w45

--- Program.fs
+++ Program.fs
@@ -80,31 +80,71 @@ module Week45 =
 
     //4.11.4
     let rec union = function
-    | x::xs,y::ys when x = y -> x :: y :: intersect (xs,ys)
-    | x::xs,y::ys when x > y -> y:: intersect (x::xs,ys)
-    | x::xs,y::ys when x < y -> x::intersect (xs,y::ys)
+    | x::xs,y::ys when x = y -> x :: y :: union (xs,ys)
+    | x::xs,y::ys when x > y -> y:: union (x::xs,ys)
+    | x::xs,y::ys when x < y -> x::union (xs,y::ys)
     | [],rest
     | rest,[] -> rest
 
     //4.11.5
+    let rec minus = function
+    | x::xs,y::ys when x = y -> minus (xs,ys)
+    | x::xs,y::ys when x > y -> minus (x::xs,ys)
+    | x::xs,y::ys when x < y -> x::minus (xs,y::ys)
+    | rest,[] -> rest
+    | [],_ -> []
+
     //4.12
+    let rec sum p list =
+        match list with
+        | x::xs ->
+            if p x then
+                x + sum p xs
+            else 
+                sum p xs
+        | [] -> 0
     //b
+    //Construct a function that takes a list of integers as input and return a reduced list of pairs representing the input. For example; input [1;3;2;2;1] will give [(1,2);(3,1);(2,2)] as output. The pairs must contain the value in the first component and the counter in the second.
+    let reduce (xs:int list) = List.countBy id xs
+
     //c
+    //If k neighbour values of an array are increasing, we say that the array contains a monotone segment of length k. For instance, the array (*) [3, 2, 1, 3, 5, 7, 9, 2, 4] contains [1, 3, 5, 7, 9] and [2, 4] as some monotone segments. Construct a function in F# computing the length of a maximal monotone segment of an array. The function should return 5 with (*) as input.
+    let maxMonotone list = list |> List.fold (fun (max,items) item -> if (List.isEmpty items) || (item > List.head items) then (max,item::items) else (Math.Max(max,List.length items), []) ) (0,[]) |> fst
+
     //d
+    //Construct a function with two parameters; a string s and a boolean ignoreSpace. The function should return true if s is a palindrome, where spaces must be ignored if and only if ignoreSpace is true.
+    let palindrome ignoreSpace (s:string)  =
+        let arr = s.ToCharArray()
+        let arr' = if ignoreSpace then arr |> Array.where (Char.IsWhiteSpace >> not) else arr
+        Array.rev arr' = arr'
     
 open Week44
 open Week45
+open FsCheck
+open System.Globalization
+
+let reversed s=
+    seq {
+            let tee = StringInfo.GetTextElementEnumerator(s)
+            while tee.MoveNext() do 
+                yield tee.GetTextElement() 
+        } |> Array.ofSeq           |> Array.rev |> String.concat ""
+
 [<EntryPoint>]
 let main argv =
     printfn "%i" (e1_4 4)
     printfn "%i" (e1_5 4)
     printfn "%i" (e2_9 (2,3))
-    FsCheck.Check.Quick (fun ls i -> (i::ls |> List.sort) = (ls |> List.sort |> Week45.insert i) )
+    Check.QuickThrowOnFailure (fun ls i -> (i::ls |> List.sort) = (ls |> List.sort |> Week45.insert i) )
     assert (intersect ([1;1;1;2;2], [1;1;2;4]) = [1;1;2])
-    FsCheck.Check.Quick (fun (xs:int list) ys ->
+    Check.QuickThrowOnFailure (fun (xs:int list) ys ->
         let xss = List.sort xs
         let yss = List.sort ys
-        (Week45.union (xss,yss)) = (List.append xss yss |> List.sort)
+        (union (xss,yss)) = (List.append xss yss |> List.sort)
         )
-
+    assert(minus([1;1;1;2;2],[1;1;2;3]) = [1;2])
+    assert(minus([1;1;2;3],[1;1;1;2;2]) = [3])
+    Check.QuickThrowOnFailure (fun p xs -> sum p xs = (xs |> List.where p |> List.sum) )
+    assert(maxMonotone [3; 2; 1; 3; 5; 7; 9; 2; 4] = 5)
+    assert(palindrome true "rad ar" )
     0 // return an integer exit code
