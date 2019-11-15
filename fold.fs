c7110c0f2959c9a1c1953b2e9e2206efeed579ad
J2ghz <j2.00ghz@gmail.com>
2019-11-15 10:22:28 +0100

    check fold

--- fold.fs
+++ fold.fs
@@ -1,15 +1,18 @@
 module fold_example
+open FsCheck
+open System.Collections.Generic
+open System
 
 let add a b = a + b
 let foldSum = List.fold add 0
 
-let avg (oldSum, oldCount) number = oldSum + number, oldCount + 1
+let avg (oldSum, oldCount) (number) = oldSum + number, oldCount + 1
 
-let foldAvg list =
-    let sum, count = List.fold avg (0,0) list
-    sum / count
+let foldAvg list=
+    let sum, count = List.fold avg (0.0,0) list
+    float sum / float count
 
 let main = 
     printfn "%i" (foldSum [1;5;4])
-    printfn "%i" (foldAvg [1;5;4])
+    Check.QuickThrowOnFailure (fun list -> foldSum list = List.sum list)
     0
